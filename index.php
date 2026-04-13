40d75d418ef845db801054af83d18d57e9452ae7
rad129ratul <ratulrs29@gmail.com>
2026-04-14 04:05:30 +0600

    Page Navigation

--- index.php
+++ index.php
@@ -27,6 +27,12 @@ get_header(); ?>
                 _e('No post found');
               endif;
             ?>
+          <div id="page_nav">
+            <?php if ('pagenav') {pagenav(); } else{ ?>
+                <?php next_posts_link(); ?>
+                <?php previous_posts_link(); ?>
+            <?php } ?>
+          </div>
         </div>
         <div class="col-md-3">
           <h2>This is sidebar area</h2>
