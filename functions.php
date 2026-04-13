3aa7b15e44e6bb0a8d019efb08057cce7369e3a8
rad129ratul <ratulrs29@gmail.com>
2026-04-13 18:10:39 +0600

    Setup Menu Position

--- functions.php
+++ functions.php
@@ -54,6 +54,29 @@ function customizar_register($wp_customize){
     'section' => 'header_area',
   ) ));
 
+  // Menu Position Option
+  $wp_customize->add_section('menu_option', array(
+    'title' => __('Menu Position Option', 'Domain'),
+    'description' => 'If you interested to change your menu position you can do it.'
+  ));
+
+  $wp_customize->add_setting('menu_position', array(
+    'default' => 'right_menu',
+  ));
+
+  $wp_customize-> add_control('menu_position', array(
+    'label' => 'Menu Position',
+    'description' => 'Select your menu position',
+    'setting' => 'menu_position',
+    'section' => 'menu_option',
+    'type' => 'radio',
+    'choices' => array(
+      'left_menu' => 'Left Menu',
+      'right_menu' => 'Right Menu',
+      'center_menu' => 'Center Menu',
+    ),
+  ));
+
 }
 
 add_action('customize_register', 'customizar_register');
--- index.php
+++ index.php
@@ -6,13 +6,13 @@ Header
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>" class="no-js">
 <head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div id="header_area">
+  <div id="header_area" class="<?php echo get_theme_mod('menu_position'); ?>">
     <div class="container">
       <div class="row">
         <div class="col-md-3">
